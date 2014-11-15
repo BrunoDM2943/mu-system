2dde2d54d424ae2785271a2a98fb27a52e8de83a
bdm2943@gmail.com <bdm2943@gmail.com@eda94469-c1a2-8665-db51-6b2136cb8673>
2014-11-15 19:37:36 +0000

    Filtro na tela de Gerencimanto

---  mu-system --username bdm2943@gmail.com/MuSystemApp/src/dao/connection/ConnectionFactory.java
+++  mu-system --username bdm2943@gmail.com/MuSystemApp/src/dao/connection/ConnectionFactory.java
@@ -3,8 +3,6 @@ package dao.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import dao.excepetions.DataAccessException;
 
